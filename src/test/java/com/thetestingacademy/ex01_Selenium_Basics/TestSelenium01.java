689e6575cfc24ad50c4a98e42671a73834d0b2ee
ATB11xAPIAutomationPrograms <dipalikunwar13@gmail.com>
2025-05-17 00:15:10 +0530

    Selenium Exercise

--- src/test/java/com/thetestingacademy/ex01_Selenium_Basics/TestSelenium01.java
+++ src/test/java/com/thetestingacademy/ex01_Selenium_Basics/TestSelenium01.java
@@ -1,4 +1,15 @@
 package com.thetestingacademy.ex01_Selenium_Basics;
 
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
 public class TestSelenium01 {
+
+    @Test
+    public void test_openVWOLoginPage(){
+
+        EdgeDriver edgeDriver = new EdgeDriver();
+        edgeDriver.get("https://app.vwo.com");
+        System.out.println(edgeDriver.getTitle());
+    }
 }
